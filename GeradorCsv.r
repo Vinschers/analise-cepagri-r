cbf7516759f71bd394defbcce86773071cbdeaad
PedroMReno <42344315+PedroMReno@users.noreply.github.com>
2020-08-31 12:00:11 -0300

    Update GeradorCsv.r
    -Lendo os dados de um csv local
    -Removendo as linhas de erro
    -Convertendo os tipos de variaveis das colunas
    -Removendo as linhas que estao fora do periodo que sera analisado

--- GeradorCsv.r
+++ GeradorCsv.r
@@ -1,10 +1,20 @@
-#Lendo os dados da internet
+#Lendo os dados do csv ja baixado
 names <- c("horario", "temp", "vento", "umid", "sensa")
-con <- url("http://ic.unicamp.br/~zanoni/cepagri/cepagri.csv")
-df <- read.csv(con, header = FALSE, sep = ";", col.names = names)
+df <- read.csv("C:\\Users\\Pedro\\Desktop\\Projetos\\analise-cepagri-r\\cepagri.csv", header = FALSE, sep = ";", col.names = names)
 
 summary(df)
 
 #Deletando variaveis que nao serao mais usadas
-rm(con)
 rm(names)
+
+#Removendo linhas que possuam valor nulo
+df <- df[df$temp != " [ERRO]", ]
+
+#Ajustando os tipos das colunas
+df$horario <- as.POSIXct(df$horario,format="%d/%m/%Y-%H:%M",tz=Sys.timezone())
+df$temp <- as.numeric(df$temp)
+
+#Removendo as linhas que nao serao analisadas
+df <- df[df$horario >= "2015-01-01" & df$horario < "2020-01-01", ]
+
+summary(df)
